c6ba0e6156695e9fb0e1a76394423fde8c04a4fa
Ivan <ivansezoskate@gmail.com>
2021-06-21 06:32:47 +0900

    Adds header button

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import food from '../../assets/food.jpg';
 import classes from './Header.module.css';
+import HeaderCartButton from './HeaderCartButton';
 
 function Header(props) {
 
@@ -9,7 +10,7 @@ function Header(props) {
         <>
             <header className={classes.header}>
                 <h1>Wonderfood</h1>
-                <button>Cart</button>
+                <HeaderCartButton />
             </header>
             <div className={classes['main-image']}>
                 <img src={food} alt="world foods" />
