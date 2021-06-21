dd543f7577f9d693eb84c02a6eedbc13d39969fd
Ivan <ivansezoskate@gmail.com>
2021-06-22 00:22:20 +0900

    Adds Meals content

---
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -0,0 +1,10 @@
+import classes from './MealItemForm.module.css';
+
+function MealItemForm(props) {
+
+    return (
+
+    );
+}
+
+export default MealItemForm;
---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,19 @@
+import classes from './MealsSummary.module.css';
+
+
+function MealsSummary() {
+
+    return (
+        <section className={classes.summary}>
+            <h2>Delicious food, delivered to you</h2>
+            <p>
+                Et ullamco anim amet officia nostrud in laboris magna. Cupidatat tempor laborum mollit nisi sint sunt commodo aliquip fugiat. 
+            </p>
+            <p>
+                Sunt fugiat do amet dolor dolore dolore consequat eiusmod do commodo ipsum proident. Ullamco irure nisi proident id dolor occaecat nulla sint occaecat Lorem ullamco.
+            </p>
+        </section>
+    )
+}
+
+export default MealsSummary;
